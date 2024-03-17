218c04709376f77dde6b09365c9612471ffeeb89
Rana-3r2m <rana3r2m@gmail.com>
2024-03-17 03:17:32 +0200

    finishing item page

--- first_task/lib/pages/item_page.dart
+++ first_task/lib/pages/item_page.dart
@@ -14,7 +14,7 @@ class ItemPage extends StatefulWidget {
 
 class _ItemPageState extends State<ItemPage> {
   int currentIndex = 0;
-  final PageController _pageController = PageController(initialPage: 0);
+  PageController _pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
     double hight = MediaQuery.of(context).size.height;
@@ -25,49 +25,113 @@ class _ItemPageState extends State<ItemPage> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Column(
-          children: [
-            SizedBox(
-              height: hight / 3,
-              width: width / 1.5,
-              child: PageView.builder(
-                  controller: _pageController,
-                  onPageChanged: (value) {
-                    setState(() {
-                      currentIndex = value;
-                    });
-                  },
-                  itemCount: 3,
-                  itemBuilder: (_, i) {
-                    return Image.asset(
-                      "assets/images/${widget.itemData.images[i]}",
-                      height: 50,
-                      // width: 12,
-                      fit: BoxFit.fill,
-                    );
-                  }),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            DotsIndicator(
-              position: currentIndex,
-              dotsCount: widget.itemData.images.length,
-              decorator: DotsDecorator(
-                color: Colors.grey,
-                activeColor: Colors.purple,
-                spacing: const EdgeInsets.only(left: 8, bottom: 10),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-                size: const Size.square(6.0),
-                activeSize: const Size(28.0, 6.0),
-                activeShape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: hight / 4,
+                width: width / 1.5,
+                child: PageView.builder(
+                    controller: _pageController,
+                    onPageChanged: (value) {
+                      setState(() {
+                        currentIndex = value;
+                      });
+                    },
+                    itemCount: 3,
+                    itemBuilder: (_, i) {
+                      return Image.asset(
+                        "assets/images/${widget.itemData.images[i]}",
+                        //height: 40,
+                        // width: 12,
+                        fit: BoxFit.fill,
+                      );
+                    }),
               ),
-            ),
-            CustomButton(title: "Add To Cart", color: Colors.yellow),
-            CustomButton(title: "Buy Now", color: Colors.orange),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        iconSize: 30,
+                        onPressed: () {
+                          if (currentIndex > 0) {
+                            currentIndex -= 1;
+                            _pageController.previousPage(
+                                duration: Duration(milliseconds: 200),
+                                curve: Curves.easeIn);
+                            setState(() {});
+                          }
+                        },
+                        icon: Icon(Icons.arrow_back)),
+                    DotsIndicator(
+                      position: currentIndex,
+                      dotsCount: widget.itemData.images.length,
+                      decorator: DotsDecorator(
+                        color: Colors.grey,
+                        activeColor: Colors.purple,
+                        spacing: const EdgeInsets.only(left: 4, right: 4),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        size: const Size.square(6.0),
+                        activeSize: const Size(28.0, 6.0),
+                        activeShape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                      ),
+                    ),
+                    IconButton(
+                        iconSize: 30,
+                        onPressed: () {
+                          if (currentIndex < 2) {
+                            currentIndex += 1;
+                            _pageController.nextPage(
+                                duration: Duration(milliseconds: 200),
+                                curve: Curves.easeIn);
+                            setState(() {});
+                          }
+                        },
+                        icon: Icon(Icons.arrow_forward)),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.itemData.name!,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text("5 satrs",
+                      style: TextStyle(
+                        fontSize: 20,
+                      )),
+                ],
+              ),
+              SizedBox(
+                width: width / 1.3,
+                child: Text(
+                  "flowerflowerflowerflowerflowerflower",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+              ),
+              CustomButton(title: "Add To Cart", color: Colors.yellow),
+              SizedBox(
+                height: 10,
+              ),
+              CustomButton(title: "Buy Now", color: Colors.orange),
+            ],
+          ),
         ),
       ),
     );
--- first_task/lib/widgets/custom_button.dart
+++ first_task/lib/widgets/custom_button.dart
@@ -6,22 +6,21 @@ class CustomButton extends StatelessWidget {
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-              height: 50,
-              width: 150,
-              child: ElevatedButton(
-                  onPressed: () {
-                  },
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(
-                          color),
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)))),
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                        fontSize: 17, color: Colors.black),
-                  )),
-            );
+    double hight = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return SizedBox(
+      height: hight / 18,
+      width: width,
+      child: ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(color),
+              shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)))),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 17, color: Colors.black),
+          )),
+    );
   }
 }
